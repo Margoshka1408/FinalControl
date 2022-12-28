b5789a13a2c62b62c513945412e890d90802130c
Margarita <margoshka_d@mail.ru>
2022-12-28 23:15:12 +0300

    Programm

--- 01/Program.cs
+++ 01/Program.cs
@@ -6,7 +6,7 @@
 //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 //[“Russia”, “Denmark”, “Kazan”] → []
 
-string[] array1 = new string[4] {"Hello", "2", "world", ":)"};//беру первый пример
+string[] array1 = new string[4] {"Hello", "2", "world", ":)"};
 string[] array2 = new string[array1.Length];
 void NewArray(string[] array1, string[] array2)
 {
